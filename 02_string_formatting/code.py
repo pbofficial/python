00e4d17ce20cc5dfca3bc9bbb4bce225cd8eff1c
Pritam Bhowmik <pritam388@gmail.com>
2021-06-23 14:50:34 +0530

    String formatting & User input

---
+++ 02_string_formatting/code.py
@@ -0,0 +1,9 @@
+name = "Bob"
+greeting = f"Hello, {name}"
+print(greeting)
+
+name = "Rolf"
+print(f"Hello, {name}")
+
+longer_phrase = "Hello {}, today is {}."
+print(longer_phrase.format("Pritam", "Wednesday"))
