378c0e4490b825db1b80a0d8fc8c7ae6849c737c
Pritam Bhowmik <pritam388@gmail.com>
2021-06-24 01:18:39 +0530

    First Python App

---
+++ 04_first_python_app/code.py
@@ -0,0 +1,7 @@
+user_age = int(input("What is your age? (in years): "))
+months = user_age * 12
+print(f"Your is age is {user_age} years or {months} months")
+
+seconds = months * 30 * 24 * 60 * 60
+
+print(f"Your is age is {user_age} years or {months} months or {seconds} seconds")
