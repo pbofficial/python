afc208900a603ff58e1577df5e0e9b410f68ec7a
Pritam Bhowmik <pritam388@gmail.com>
2021-07-04 19:58:40 +0530

    Boolean operations

---
+++ 07_booleans/code.py
@@ -0,0 +1,12 @@
+#Comparisons: ==, !=, >, <, >=, <=
+
+print(5 == 5)
+print("5" == 5)
+print("5" != 5)
+print("5" == "5")
+
+friends = ['Rolf', 'Bob']
+abroad = ['Rolf', 'Bob']
+
+print(friends == abroad) #value check -> True
+print(friends is abroad) #address check -> False
